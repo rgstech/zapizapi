00698b28b58363a5ce3dbfedfdc29166856d6094
rgstech <guimaorod@gmail.com>
2024-07-17 22:13:31 -0300

    fucntion prefix updated to make sense.

--- include/functions.php
+++ include/functions.php
@@ -1,20 +1,20 @@
 <?php
 
-function sp_load_frontend_styles()
+function zapi_load_frontend_styles()
 {
     wp_register_style( 'zapi-styles',  plugins_url( '../css/frontend.css', __FILE__ ) );
     wp_enqueue_style( 'zapi-styles' );
 }
 
-function my_menu_pages()
+function zapi_menu_pages()
 {
 
-    add_menu_page('Configurar zapizapi', 'ZapiZapi', 'manage_options', 'my-menu', 'sp_panel_page');
+    add_menu_page('Configurar zapizapi', 'ZapiZapi', 'manage_options', 'my-menu', 'zapi_panel_page');
 
 }
 
 // function to load admin page
-function sp_panel_page() 
+function zapi_panel_page() 
 {
     
     require_once dirname( __DIR__ ) . "/admin/panel.php";
--- include/init.php
+++ include/init.php
@@ -2,8 +2,8 @@
 
 
 
-add_action('wp_enqueue_scripts', 'sp_load_frontend_styles');
+add_action('wp_enqueue_scripts', 'zapi_load_frontend_styles');
 
 add_action('wp_footer', 'zapi_icon_show');
 
-add_action( 'admin_menu', 'my_menu_pages');
+add_action( 'admin_menu', 'zapi_menu_pages');

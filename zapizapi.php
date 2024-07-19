f0c0c89a4a0538996c6fd428d02408743b9b7c43
rgstech <guimaorod@gmail.com>
2024-07-19 00:27:04 -0300

    adicionado codigo de proteção de acesso direto/ access direct protection code added

--- zapizapi.php
+++ zapizapi.php
@@ -11,6 +11,15 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
+if ( ! function_exists( 'add_filter' ) ) { // Protege contra acesso direto verificando se qualquer função nativa do wordpres foi definida anteriormente
+                                           // Protects direct file access verifying if some wp native function is defined 
+
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+
+}
+
 define('ZP_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
 
 define('ZP_PLUGIN_URL', plugin_dir_url( __FILE__ ));
